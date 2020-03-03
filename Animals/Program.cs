bd1f849417dc4abb081c1db4d531c3dd40e983fd
Maria <mariabrock414@gmail.com>
2020-03-02 21:19:24 -0600

    Add project files.

---
+++ Animals/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Animals.Reptiles;
+
+
+namespace Animals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instanstaciation
+            var americanAlligator = new Alligator(100);
+            americanAlligator.IsAquatic = true;
+
+            var africanAlligator = new Alligator(2);
+            africanAlligator.IsAquatic = false;
+
+            americanAlligator.Run(75);
+            africanAlligator.Run(4);
+
+            americanAlligator.Run(40);
+        }
+    }
+}
---
+++ Animals/Reptiles/Alligator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals.Reptiles
+{
+    class Alligator
+    {
+        //private bool _isAquatic;
+        //this is a property, they expose information
+        public bool IsAquatic { get; set; }
+
+        //this is a field, it will be informtion just for us, others will not be able to see it.
+        private int _stepGoal;
+        private int _stepCount;
+
+        //this is a constructor, the name and type it returns are the same, soy ou will only see the access modifier and name
+        public Alligator(int stepGoal)
+        {
+            IsAquatic = true;
+            _stepGoal = stepGoal;
+            
+        }
+
+        //access modifier, return type (what value the method returns), name, and parameters make up a method
+        public void Grunt()
+        {
+            //now you've written a method
+
+            if (IsAquatic)
+            {
+                Console.WriteLine("Bubble Bubble");
+            }
+            else
+            {
+                Console.WriteLine("grrrrrrr!");
+            }
+            
+        }
+
+        //declaring parameters within a C# method
+        public void Run(int steps)
+        {
+            _stepCount += steps;
+
+            //string interpolation adds a $ to the front of the string
+            Console.WriteLine($"You ran {steps} steps.");
+
+            if (_stepCount >= _stepGoal)
+            {
+                Console.WriteLine("Congrats! You met you goal.");
+            }
+        }
+    }
+}
